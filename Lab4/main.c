5f262c88a9955d995ddb26e1496769b94c5c137e
ETFBL <nickyleeyy@linuxmail.org>
2015-12-18 14:40:50 +0100

    prva verzija projekta

---
+++ Lab4/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Student
+ *
+ * Created on December 18, 2015, 2:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Lab 4");
+    return (EXIT_SUCCESS);
+}
+
